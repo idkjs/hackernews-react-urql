22f0707f0947a05d95cf0641ddf428a7c02b8caa
Alain Armand <aarmand.inbox@gmail.com>
2019-09-03 12:46:34 -0400

    createLink.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import LinkList from './LinkList'
+import CreateLink from './CreateLink'
 
-const App = () => <LinkList />
+const App = () => <CreateLink />
+// const App = () => <LinkList />
 
 export default App
