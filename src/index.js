76ee506825edcebd30d102c0c1b6d419818bcd5b
Alain Armand <aarmand.inbox@gmail.com>
2019-09-03 12:14:00 -0400

    working default-exchanges

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './components/App'
+import { cacheExchange } from '@urql/exchange-graphcache'
+import { Provider, Client, defaultExchanges } from 'urql'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new Client({
+  url: 'http://localhost:4000',
+  exchanges: defaultExchanges
+})
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const cache = cacheExchange({})
+
+// const client = new Client({
+//   url: 'http://localhost:4000',
+//   exchanges: [dedupExchange, cache, fetchExchange],
+// })
+
+ReactDOM.render(
+  <Provider value={client}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+
